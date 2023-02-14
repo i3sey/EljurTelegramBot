81767a68a0faa778dc4f0a8e517dc16ba7038a41
i3sey <deniskedrovsky@mail.ru>
2023-02-15 01:15:56 +0500

    algorithm for processing pages and numbers

---
+++ bot/functions/hwSend.py
@@ -0,0 +1,28 @@
+import re
+import json
+
+async def hwSend(text):
+    page = ''
+    num = ''
+    match = re.search('<b>(.*?)</b>', text)
+    if match:
+        subject = match.group(1)
+    matches = re.findall('<code>(.*?)</code>', text)
+    if len(matches) >= 2:
+        hw = json.loads(matches[1].replace("'", '"'))
+    if (len(hw['page']) if "page" in hw else 0) > 1 and (len(hw['number']) if "number" in hw else 2) <= 1:
+        for i in hw['page']:
+            page = page + ', ' + i
+        return f'Предмет - <b>{subject}</b>, ищу гдз{" страница " + page if "page" in hw else ""}{", номер " + hw["number"][0] if "number" in hw else ""}'
+    elif (len(hw['number']) if "number" in hw else 0) > 1 and (len(hw['page']) if "page" in hw else 2) <= 1:
+        for i in hw['number']:
+            num = num + ', ' + i
+        return f'Предмет - <b>{subject}</b>, ищу гдз{" страница " + hw["page"][0] if "page" in hw else ""}{", номер " + num if "number" in hw else ""}'
+    elif (len(hw['number']) if "number" in hw else 0) > 1 and (len(hw['page']) if "page" in hw else 0) > 1:
+        for i in hw['page']:
+            page = page + ', ' + i
+        for i in hw['number']:
+            num = num + ', ' + i
+        return f'Предмет - <b>{subject}</b>, ищу гдз{" страница " + page if "page" in hw else ""}{", номер " + num if "number" in hw else ""}'
+    else:
+        return f'Предмет - <b>{subject}</b>, ищу гдз{" страница " + hw["page"][0] if "page" in hw else ""}{", номер " + hw["number"][0] if "number" in hw else ""}'
