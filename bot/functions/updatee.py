50a39cb8d98b37d401524e82f57311b1c847bb71
i3sey <deniskedrovsky@mail.ru>
2023-03-03 22:39:54 +0500

    update

--- bot/functions/updatee.py
+++ bot/functions/updatee.py
@@ -1,5 +1,18 @@
 import subprocess
 
 def updateq():
-    result = subprocess.run(['git', 'pull'], stdout=subprocess.PIPE)
-    return result.stdout.decode('utf-8')
+    result = ""
+    try:
+        # Выполняем команду git pull в терминале
+        p1 = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = p1.communicate() # выводим результат выполнения из терминала
+        result += output.decode("utf-8") + "\n" + error.decode("utf-8")
+        
+        # После успешного выполнения первой команды - выполняем pm2 restart run
+        if not error:
+            p2 = subprocess.Popen(["pm2", "restart", "run"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, error = p2.communicate() # выводим результат выполнения из терминала
+            result += output.decode("utf-8") + "\n" + error.decode("utf-8")
+    except Exception as e:
+        result = "An error occurred while executing the command: {}\n{}".format(str(e), result)
+    return result
