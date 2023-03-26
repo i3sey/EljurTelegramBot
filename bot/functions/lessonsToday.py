66f68b86d61d8312a814d92570be06af283853cf
i3sey <deniskedrovsky@mail.ru>
2023-03-26 21:24:07 +0500

    update for files test

--- bot/functions/lessonsToday.py
+++ bot/functions/lessonsToday.py
@@ -1,10 +1,8 @@
 import datetime
-
 from bot.functions.api import idJournal
 from bot.misc.util import dailyCleanup
 
-
-async def todaylessons(msg):
+def todaylessons(msg):
     time_zone = datetime.timezone(datetime.timedelta(hours=5))
     date = datetime.datetime.now(time_zone)
     day = date.strftime("%d")
@@ -12,8 +10,9 @@ async def todaylessons(msg):
     dictionary = idJournal(msg.chat.id, 0)
     if dictionary == 1:
         return 'ошибка, попробуй войти с помощью команды /editData или напиши Денису'
-    for key, val in dictionary.items():
-        if val['date'] == date_str:
-            today = key
-            break
-    return f'Сегодня <b>{today}, {date_str}</b>\n{dailyCleanup(dictionary[today]["lessons"])}'
+    today = next((key for key, val in dictionary.items() if val['date'] == date_str), None)
+    if today:    
+        lessone, urls = dailyCleanup(dictionary[today]["lessons"])
+        return f'Сегодня <b>{today}, {date_str}</b>\n{lessone}', urls
+    else:
+        return f'Уроков нет', {}
