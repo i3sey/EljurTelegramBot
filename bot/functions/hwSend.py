b0a26762308754cc19255bdb0e37ed5d878831bf
i3sey <86531179+i3sey@users.noreply.github.com>
2023-04-06 17:50:48 +0500

    Update hwSend.py

--- bot/functions/hwSend.py
+++ bot/functions/hwSend.py
@@ -2,8 +2,8 @@ import re
 import json
 
 async def hwSend(text):
-    page = ''
-    num = ''
+    page = []
+    num = []
     match = re.search('<b>(.*?)</b>', text)
     if match:
         subject = match.group(1)
@@ -16,13 +16,13 @@ async def hwSend(text):
         return f'Предмет - <b>{subject}</b>, ищу гдз{" страница " + page if "page" in hw else ""}{", номер " + hw["number"][0] if "number" in hw else ""}', page, hw["number"][0] if "number" in hw else "", subject
     elif (len(hw['number']) if "number" in hw else 0) > 1 and (len(hw['page']) if "page" in hw else 2) <= 1:
         for i in hw['number']:
-            num = num + ', ' + i
-        return f'Предмет - <b>{subject}</b>, ищу гдз{" страница " + hw["page"][0] if "page" in hw else ""}{", номер " + num if "number" in hw else ""}', hw["page"][0] if "page" in hw else "", num, subject
+            num.append(i)
+        return f'Предмет - <b>{subject}</b>, ищу гдз{" страница " + hw["page"][0] if "page" in hw else ""}{", номер " + num[0] if "number" in hw else ""}', hw["page"][0] if "page" in hw else "", num, subject
     elif (len(hw['number']) if "number" in hw else 0) > 1 and (len(hw['page']) if "page" in hw else 0) > 1:
         for i in hw['page']:
             page = page + ', ' + i
         for i in hw['number']:
-            num = num + ', ' + i
+            num.append(i)
         return f'Предмет - <b>{subject}</b>, ищу гдз{" страница " + page if "page" in hw else ""}{", номер " + num if "number" in hw else ""}', page, num, subject
     else:
         return f'Предмет - <b>{subject}</b>, ищу гдз{" страница " + hw["page"][0] if "page" in hw else ""}{", номер " + hw["number"][0] if "number" in hw else ""}', hw["page"][0] if "page" in hw else "", hw["number"][0] if "number" in hw else "", subject
